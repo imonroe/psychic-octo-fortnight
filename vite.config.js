35105971c676d9a3867f3a66a248e25c37574ea9
Ian Monroe <ian@ianmonroe.com>
2024-04-09 20:57:50 +0000

    installed breeze

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,14 @@
-import {defineConfig} from 'vite'
-import laravel from 'laravel-vite-plugin'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-const extendedViteDevServerOptions = {}
-
-if (process.env.GITPOD_VITE_URL) {
-    extendedViteDevServerOptions.hmr = {
-        protocol: 'wss',
-        host: new URL(process.env.GITPOD_VITE_URL).hostname,
-        clientPort: 443
-    }
-}
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        ...extendedViteDevServerOptions
-    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
-})
+});
